007b35aa9621ad0703b948ea71e76d75aa1b4758
Krakkev <kevangy@hotmail.com>
2018-08-10 13:23:55 -0500

    new version

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ import setuptools
 setuptools.setup(
     name = 'ByRequests',
     packages = ['ByRequests'], # this must be the same as the name above
-    version = '1.1.3',
+    version = '1.1.4',
     description = 'Helper to use proxy services with Requests',
     author = 'Kevin B. Garcia Alonso',
     author_email = 'kevangy@hotmail.com',
     url = 'https://github.com/Krakkev/ByRequests', # use the URL to the github repo
-    download_url = 'https://github.com/Krakkev/ByRequests/tarball/1.1.3',
+    download_url = 'https://github.com/Krakkev/ByRequests/tarball/1.1.4',
     keywords = ['requests', 'request', 'proxy', 'proxies', 'beautifulsoap', 'xpath', 'GET', 'POST'],
     install_requires=[
         'beautifulsoup4>=4.6.0',
         'bs4>=0.0.1',
-        'requests>=2.18.1',
+        'requests>=2.18.0',
         'urllib3>=1.21.1',
         'fake-useragent>=0.1.10',
         'lxml>=4.1.0',
